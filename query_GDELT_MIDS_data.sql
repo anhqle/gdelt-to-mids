079b927eae8f418686c99705eb7767e99b89b526
Anh Le <aql3@duke.edu>
2014-01-16 14:17:46 -0500

    my own first commit

---
+++ query_GDELT_MIDS_data.sql
@@ -0,0 +1,26 @@
+SHOW FIELDS IN gdelt_historical;
+
+SELECT * FROM gdelt_historical
+    WHERE
+    SQLDATE=20120101 AND
+    Actor1Code="IGO";
+
+SELECT * FROM gdelt_historical
+    WHERE
+    MonthYear=201201 AND
+    Actor1Code="IGO";
+
+SELECT * FROM gdelt_historical
+    WHERE
+    MonthYear=201202;
+
+SELECT * FROM gdelt_historical
+    WHERE
+    SQLDATE=20120101 AND
+    Actor2Code = "VIE";
+
+SELECT * FROM gdelt_historical
+    WHERE 
+    SQLDATE=20111111 AND
+    (Actor2Code="VIE" OR
+    Actor1Code="VIE");
