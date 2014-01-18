1fb38adb57d907f7d0d41867a938d4dfe33dfcf0
LaDilettante <aql3@duke.edu>
2014-01-17 22:19:38 -0500

    Got data for democracy_cluster word analysis

--- query_GDELT_MIDS_data.sql
+++ query_GDELT_MIDS_data.sql
@@ -1,26 +1,29 @@
 SHOW FIELDS IN gdelt_historical;
 
+# Get data to test merge MIDS and GDELT, May 2005
 SELECT * FROM gdelt_historical
-    WHERE
-    SQLDATE=20120101 AND
-    Actor1Code="IGO";
+	WHERE
+	MonthYear="200505";
 
-SELECT * FROM gdelt_historical
-    WHERE
-    MonthYear=201201 AND
-    Actor1Code="IGO";
+# Get stories data for the Polity project, Brazil Dec 2012
+SELECT @Brazil_id := `actor_id` FROM dict_actors
+	WHERE name="Brazil" AND
+	is_country=1;
 
-SELECT * FROM gdelt_historical
-    WHERE
-    MonthYear=201202;
+SHOW FIELDS FROM events;
+SELECT @story_id := `story_id` FROM events
+	WHERE
+	(source_actor_id=@Brazil_id OR
+	target_actor_id=@Brazil_id) AND
+	YEAR(event_date)=2012;
 
-SELECT * FROM gdelt_historical
-    WHERE
-    SQLDATE=20120101 AND
-    Actor2Code = "VIE";
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_table AS (SELECT DISTINCT story_id FROM events
+	WHERE
+	(source_actor_id=@Brazil_id OR
+	target_actor_id=@Brazil_id) AND
+	YEAR(event_date)=2012);
 
-SELECT * FROM gdelt_historical
-    WHERE 
-    SQLDATE=20111111 AND
-    (Actor2Code="VIE" OR
-    Actor1Code="VIE");
+SELECT * FROM temp_table;
+SELECT * FROM stories, temp_table
+	WHERE
+	stories.StoryID = temp_table.story_id;
